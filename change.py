062e6b3e3509db57bc1324c02359621948d30014
ilmir Sharifullin <ilmirsharifullin2000@gmail.com>
2022-02-27 14:35:27 +0300

    Изменения (Латте Макиато)

--- change.py
+++ change.py
@@ -1,21 +1,23 @@
 import sqlite3
 import sys
 
-from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+
+from UI.addEditCoffeeForm import Ui_MainWindow
 
 
 class ChangeWidget(QMainWindow):
     def __init__(self):
-        super().__init__()
-        uic.loadUi('addEditCoffeeForm.ui', self)
-        self.con = sqlite3.connect('coffee.sqlite')
+        super(ChangeWidget, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.con = sqlite3.connect('data/coffee.sqlite')
         self.select_data()
-        self.search.editingFinished.connect(self.searching)
-        self.rbtn_id.pressed.connect(self.id_pressed)
-        self.rbtn_name.pressed.connect(self.name_pressed)
-        self.save.clicked.connect(self.save_info)
+        self.ui.search.editingFinished.connect(self.searching)
+        self.ui.rbtn_id.pressed.connect(self.id_pressed)
+        self.ui.rbtn_name.pressed.connect(self.name_pressed)
+        self.ui.save.clicked.connect(self.save_info)
         self.by_id = True
         self.new = True
 
@@ -23,21 +25,21 @@ class ChangeWidget(QMainWindow):
         query = 'Select * from coffee'
         a = ['ID', 'Название']
         res = self.con.cursor().execute(query).fetchall()
-        self.tableWidget.setColumnCount(2)
-        self.tableWidget.setRowCount(0)
+        self.ui.tableWidget.setColumnCount(2)
+        self.ui.tableWidget.setRowCount(0)
         for i in range(2):
-            self.tableWidget.setHorizontalHeaderItem(i, QTableWidgetItem(a[i]))
+            self.ui.tableWidget.setHorizontalHeaderItem(i, QTableWidgetItem(a[i]))
         for i, row in enumerate(res):
-            self.tableWidget.setRowCount(
-                self.tableWidget.rowCount() + 1)
+            self.ui.tableWidget.setRowCount(
+                self.ui.tableWidget.rowCount() + 1)
             for j, elem in enumerate(row):
-                self.tableWidget.setItem(
+                self.ui.tableWidget.setItem(
                     i, j, QTableWidgetItem(str(elem)))
 
     def searching(self):
-        text = self.search.text()
+        text = self.ui.search.text()
         if text:
-            self.save.setEnabled(True)
+            self.ui.save.setEnabled(True)
         else:
             return
         if self.by_id:
@@ -49,21 +51,21 @@ class ChangeWidget(QMainWindow):
         except Exception:
             res = None
         if res:
-            self.name.setText(res[1])
-            self.roastlevel.setText(res[2])
-            self.condition.setValue(res[3])
-            self.description.setPlainText(res[4])
-            self.price.setText(str(res[5]))
-            self.volume.setText(str(res[6]))
+            self.ui.name.setText(res[1])
+            self.ui.roastlevel.setText(res[2])
+            self.ui.condition.setValue(res[3])
+            self.ui.description.setPlainText(res[4])
+            self.ui.price.setText(str(res[5]))
+            self.ui.volume.setText(str(res[6]))
             self.new = False
         else:
             self.new = True
-            self.name.setText('')
-            self.roastlevel.setText('')
-            self.condition.setValue(0)
-            self.description.setPlainText('')
-            self.price.setText('')
-            self.volume.setText('')
+            self.ui.name.setText('')
+            self.ui.roastlevel.setText('')
+            self.ui.condition.setValue(0)
+            self.ui.description.setPlainText('')
+            self.ui.price.setText('')
+            self.ui.volume.setText('')
 
     def id_pressed(self):
         self.by_id = True
@@ -74,19 +76,19 @@ class ChangeWidget(QMainWindow):
     def save_info(self):
         if self.new:
             try:
-                query = f'''INSERT INTO coffee(name, [roast level], condition, description, price, [packing volume]) VALUES('{self.name.text()}', '{self.roastlevel.text()}',
-                                                        {self.condition.value()}, '{self.description.toPlainText()}',
-                                                        {float(self.price.text())}, {float(self.volume.text())})'''
+                query = f'''INSERT INTO coffee(name, [roast level], condition, description, price, [packing volume]) VALUES('{self.ui.name.text()}', '{self.ui.roastlevel.text()}',
+                                                        {self.ui.condition.value()}, '{self.ui.description.toPlainText()}',
+                                                        {float(self.ui.price.text())}, {float(self.ui.volume.text())})'''
                 self.con.cursor().execute(query)
             except Exception as e:
                 raise e
         else:
             try:
-                query = f'''UPDATE coffee SET name='{self.name.text()}', [roast level]='{self.roastlevel.text()}', condition={self.condition.value()}, description='{self.description.toPlainText()}', price={float(self.price.text())}, [packing volume]={float(self.volume.text())}'''
+                query = f'''UPDATE coffee SET name='{self.ui.name.text()}', [roast level]='{self.ui.roastlevel.text()}', condition={self.ui.condition.value()}, description='{self.ui.description.toPlainText()}', price={float(self.ui.price.text())}, [packing volume]={float(self.ui.volume.text())}'''
                 if self.by_id:
-                    query += f''' WHERE id={self.search.text()} '''
+                    query += f''' WHERE id={self.ui.search.text()} '''
                 else:
-                    query += f''' WHERE name like '%{self.search.text()}%' '''
+                    query += f''' WHERE name like '%{self.ui.search.text()}%' '''
                 self.con.cursor().execute(query)
             except Exception as e:
                 raise e
--- main.py
+++ main.py
@@ -5,34 +5,37 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
 
+from UI.main_design import Ui_MainWindow
+
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('main.ui', self)
-        self.connection = sqlite3.connect("coffee.sqlite")
-        self.pushButton.clicked.connect(self.select_data)
-        self.textEdit.setPlainText("SELECT * FROM coffee")
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.connection = sqlite3.connect("data/coffee.sqlite")
+        self.ui.pushButton.clicked.connect(self.select_data)
+        self.ui.textEdit.setPlainText("SELECT * FROM coffee")
         self.select_data()
 
     def select_data(self):
-        query = self.textEdit.toPlainText()
+        query = self.ui.textEdit.toPlainText()
         a = ['ID', 'Название', 'Степень обжарки', 'Состояние', 'Описание', 'Цена (руб.)', 'Объем упаковки (г.)']
         res = self.connection.cursor().execute(query).fetchall()
-        self.tableWidget.setColumnCount(7)
-        self.tableWidget.setRowCount(0)
+        self.ui.tableWidget.setColumnCount(7)
+        self.ui.tableWidget.setRowCount(0)
         for i in range(7):
-            self.tableWidget.setHorizontalHeaderItem(i, QTableWidgetItem(a[i]))
+            self.ui.tableWidget.setHorizontalHeaderItem(i, QTableWidgetItem(a[i]))
         for i, row in enumerate(res):
-            self.tableWidget.setRowCount(
-                self.tableWidget.rowCount() + 1)
+            self.ui.tableWidget.setRowCount(
+                self.ui.tableWidget.rowCount() + 1)
             for j, elem in enumerate(row):
                 if j == 3:
                     if elem == 1:
                         elem = 'В зернах'
                     elif elem == 0:
                         elem = 'Растворимый'
-                self.tableWidget.setItem(
+                self.ui.tableWidget.setItem(
                     i, j, QTableWidgetItem(str(elem)))
 
     def closeEvent(self, event):
